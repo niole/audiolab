17fbfa0410a33c373b56b7e2311a238abf638454
niole <niolenelson@gmail.com>
2019-07-14 13:27:11 -0700

    Add data rep of parsed audio, add basic view

---
+++ src/domain/ParsedAudio.ts
@@ -0,0 +1,8 @@
+export type ParsedAudio = {
+  startTime: number;
+  endTime: number;
+  audioFileURI: string;
+  creatorId: string;
+  id: string;
+  speakerId: string;
+};
---
+++ src/views/parsedaudio/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class ParsedAudio extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                slf
+            </div>
+        );
+    }
+}
